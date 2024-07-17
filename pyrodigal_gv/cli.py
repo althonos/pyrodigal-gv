32a06bbf18186e4ba00a74aa8b04fe917d9f843b
Martin Larralde <martin.larralde@embl.de>
2024-07-17 22:31:45 +0200

    Bump `pyrodigal_gv.cli` to support release `v3.5` of Pyrodigal

--- pyrodigal_gv/cli.py
+++ pyrodigal_gv/cli.py
@@ -9,8 +9,10 @@ from . import ViralGeneFinder, __version__
 def argument_parser(
     prog: str = "pyrodigal-gv",
     version: str = __version__,
+    input_required: bool = True,
+    formatter_class: argparse.HelpFormatter = argparse.ArgumentDefaultsHelpFormatter,
 ) -> argparse.ArgumentParser:
-    parser = _argument_parser(prog=prog, version=version)
+    parser = _argument_parser(prog=prog, version=version, input_required=input_required, formatter_class=formatter_class)
     mode_action = next( action for action in parser._actions if action.dest == "p" )
     mode_action.default = "meta"
     return parser
@@ -20,14 +22,18 @@ def main(
     argv: typing.Optional[typing.List[str]] = None,
     stdout: typing.TextIO = sys.stdout,
     stderr: typing.TextIO = sys.stderr,
+    stdin: typing.TextIO = sys.stdin,
     *,
     gene_finder_factory: typing.Callable[..., GeneFinder] = ViralGeneFinder,
     argument_parser: typing.Callable[[], argparse.ArgumentParser] = argument_parser,
+    formatter_class: argparse.HelpFormatter = argparse.ArgumentDefaultsHelpFormatter,
 ) -> int:
     return _main(
         argv,
         stdout=stdout,
         stderr=stderr,
+        stdin=stdin,
         gene_finder_factory=gene_finder_factory,
-        argument_parser=argument_parser
+        argument_parser=argument_parser,
+        formatter_class=formatter_class,
     )
